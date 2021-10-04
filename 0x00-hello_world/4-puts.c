0df5ff2c5180c3b4200af8395397ccf0c556a857
3985martacano <3985@holbertonschool.com>
2021-10-03 20:50:49 -0700

    intento 3

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - is a function,all commands and text must into this function
+ *
+ * Return: Always 0(success)
+ * /
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
