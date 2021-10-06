b256da96f03a3aa018773ace8cc3d2367c359feb
3985martacano <3985@holbertonschool.com>
2021-10-06 09:05:34 -0700

    lectura de errores

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  * Return: 0
  *
  **/
-void print_alphabet(void);
+void print_alphabet(void)
 {
 int d;
 
@@ -12,5 +12,5 @@ for (d = 97; d <= 122; d++)
 {
 _putchar(d);
 }
-_putchar('\n')
+_putchar('\n');
 }
