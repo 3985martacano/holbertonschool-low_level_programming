99eb191077fd9b7f8f4b5b7e4ae2ececc5ba09dc
3985martacano <3985@holbertonschool.com>
2021-10-05 21:53:53 -0700

    intento del 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: 0
+ *
+ **/
+int main(void)
+{
+char c[] = "abcdefghijklmnopqrstvwxyz";
+int d;
+
+for (d = 0; d <= 25; d++)
+{
+_putchar(c[d]);
+}
+_putchar('\n');
+return (0);
+}
+
