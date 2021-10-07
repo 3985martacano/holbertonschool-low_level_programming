2f9908c4686f001a545891a217b6eddc09c63d8e
3985martacano <3985@holbertonschool.com>
2021-10-06 21:21:32 -0700

    comentario del prptopype

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *print_alphabet - alphabetx10.
+ *void print_alphabet_x10 - alphabetx10.
  *
  *return 0
  *
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  - check the code.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c);
+{
+	for (d = 'a'; d <= 'z'; d++)
+
+	if (islower(c))
+}
+{
+
+	 _putchar('\n');
+}
